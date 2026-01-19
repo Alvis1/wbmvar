a466e338980423968db610ac2b595829e111bfbb
OtoJauja <97533069+OtoJauja@users.noreply.github.com>
2026-01-19 19:33:33 +0200

    Added circles to indicate when to hit

---
+++ OtoJauja/note-sequence.js
@@ -0,0 +1,13 @@
+// Beethoven Symphony No. 5 - 1st Movement (Allegro con brio)
+
+window.TIMPANI_NOTE_SEQUENCE = [
+  { time: 2.22, drumIndex: 0 }, // C
+  { time: 2.78, drumIndex: 0 },
+  { time: 3.33, drumIndex: 1 }, // G
+  { time: 3.89, drumIndex: 1 },
+
+  { time: 6.67, drumIndex: 0 },
+  { time: 7.22, drumIndex: 0 },
+  { time: 7.78, drumIndex: 1 },
+  { time: 8.33, drumIndex: 1 },
+];
