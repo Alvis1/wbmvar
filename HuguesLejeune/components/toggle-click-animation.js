14d07daed5e9de21f64d00ad9d76107f1457ad12
HuguesLej <hugues.lejeune@epitech.eu>
2026-01-22 01:07:17 +0200

    Hugues: visible info panels are now hidden when another panel is set to visible to avoid user confusion

--- HuguesLejeune/components/toggle-click-animation.js
+++ HuguesLejeune/components/toggle-click-animation.js
@@ -74,3 +74,53 @@ AFRAME.registerComponent("toggle-click-target-animation", {
         });
     },
 });
+
+AFRAME.registerComponent("toggle-panel-visibility", {
+    multiple: true,
+
+    schema: {
+        name: { type: "string", default: "" },
+        target: { type: "selector" },
+        property: { type: "string", default: "" },
+        from: { type: "string", default: "" },
+        to: { type: "string", default: "" },
+        dur: { type: "number", default: 500 },
+        easing: { type: "string", default: "easeInOutCirc" },
+    },
+
+    init() {
+        this.toggled = false;
+
+        this.data.target.addEventListener("reset-target", (event) => {
+            if (this.toggled) {
+                this.el.emit("click");
+            }
+        });
+
+        this.el.addEventListener("click", () => {
+            const to = this.toggled ? this.data.from : this.data.to;
+
+            this.data.target.setAttribute(
+                `animation__${this.data.name}`,
+                `property: ${this.data.property}; to: ${to}; dur: ${this.data.dur}; easing: ${this.data.easing}`,
+            );
+            this.toggled = !this.toggled;
+
+            if (this.data.property !== "material.opacity") {
+                return;
+            }
+            if (to == 0) {
+                setTimeout(() => {
+                    this.data.target.setAttribute("visible", false);
+                }, this.data.dur);
+            } else {
+                this.data.target.setAttribute("visible", true);
+                document.querySelectorAll(`.${this.data.target.className}`).forEach((el) => {
+                    if (el !== this.data.target) {
+                        el.dispatchEvent(new Event("reset-target"));
+                    }
+                });
+            }
+        });
+    },
+});
