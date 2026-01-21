9ec5371c7a285b5d385ca26d2c105e15a2a4f943
HuguesLej <hugues.lejeune@epitech.eu>
2026-01-21 18:37:17 +0200

    Hugues: add toggle-click-target-animation component

--- HuguesLejeune/components/toggle-click-animation.js
+++ HuguesLejeune/components/toggle-click-animation.js
@@ -21,3 +21,29 @@ AFRAME.registerComponent('toggle-click-animation', {
         });
     },
 });
+
+AFRAME.registerComponent('toggle-click-target-animation', {
+    schema: {
+        target: {type: 'selector'},
+        property: {type: 'string', default: ''},
+        from: {type: 'string', default: ''},
+        to: {type: 'string', default: ''},
+        dur: {type: 'number', default: 500},
+        easing: {type: 'string', default: 'easeInOutCirc'},
+    },
+
+    init() {
+        this.toggled = false;
+
+        this.el.addEventListener('click', () => {
+            const to = this.toggled ? this.data.from : this.data.to;
+
+            this.data.target.setAttribute(
+                'animation',
+                `property: ${this.data.property}; to: ${to}; dur: ${this.data.dur}; easing: ${this.data.easing}`,
+            );
+            this.toggled = !this.toggled;
+        });
+    },
+});
+
