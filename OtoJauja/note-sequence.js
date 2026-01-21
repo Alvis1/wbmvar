a2bc2fbedefce8ccaa5d94d2cb09fc11244050b0
OtoJauja <97533069+OtoJauja@users.noreply.github.com>
2026-01-21 19:22:50 +0200

    Some stuff changed and added menu stuff

--- OtoJauja/note-sequence.js
+++ OtoJauja/note-sequence.js
@@ -1,13 +1,16 @@
 // Beethoven Symphony No. 5 - 1st Movement (Allegro con brio)
+// 108 BPM
+// Measure time 2/4 = ~1.11s
+// Quarter note = ~0.55s
+// Eith note = ~0.277s
+// Timpani - 1 = G
+// Timpani - 2 = C
 
 window.TIMPANI_NOTE_SEQUENCE = [
-  { time: 2.22, drumIndex: 0 }, // C
-  { time: 2.78, drumIndex: 0 },
-  { time: 3.33, drumIndex: 1 }, // G
-  { time: 3.89, drumIndex: 1 },
-
-  { time: 6.67, drumIndex: 0 },
-  { time: 7.22, drumIndex: 0 },
-  { time: 7.78, drumIndex: 1 },
-  { time: 8.33, drumIndex: 1 },
+  { time: 22.43, drumIndex: 0 }, // G
+  { time: 22.7, drumIndex: 0 },
+  { time: 22.97, drumIndex: 0 },
+  { time: 23.24, drumIndex: 1 }, // C
+  { time: 23.79, drumIndex: 1 },
+  { time: 24.34, drumIndex: 0 }, // G
 ];
